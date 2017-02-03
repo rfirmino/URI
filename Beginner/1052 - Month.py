27329a496d4d67a2b7198645436d00d40ae88475
Rafael Firmino Magalhães <baratacnsp@yahoo.com.br>
2017-02-03 10:52:20 -0200

    1052 - Mês

---
+++ Beginner/1052 - Month.py
@@ -0,0 +1,6 @@
+v = input()
+v = v.split()
+n = int(v[0])
+dict = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September',
+10: 'October', 11: 'November', 12: 'December'}
+print(dict[n])
